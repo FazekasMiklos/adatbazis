6f8f0c0b82f6147e0227be74c19f95c183be2a1e
FazekasMiklos <miklos81006@gmail.com>
2021-10-21 12:10:47 +0200

    belépés

---
+++ model/ulesrend.php
@@ -0,0 +1,50 @@
+<?php
+
+class ulesrend {
+    
+    private $id;
+    private $nev;
+    private $sor;
+    private $oszlop;
+    private $jelszo;
+    private $felhasznalonev;
+
+    public function set_user($id, $conn) {
+        // adatbázisból lekérdezzük
+        $sql = "SELECT id, nev, sor, oszlop, jelszo, felhasznalonev FROM ulesrend";
+        $sql .= " WHERE id = $id ";
+        $result = $conn->query($sql);
+        
+        if($conn->query($sql)){
+        if($result->num_rows > 0){
+            $row = $result->fetch_assoc();
+            $this->id = $row['id'];
+            $this->nev = $row['nev'];
+            $this->sor = $row['sor'];
+            $this->oszlop = $row['oszlop'];
+            $this->jelszo = $row['jelszo'];
+            $this->felhasznalonev = $row['felhasznalonev'];
+        }
+    }
+    else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+    // építsük fel az összes get metódust
+    public function get_nev() {  
+        return $this->nev;
+    }
+    public function get_jelszo() {  
+        return $this->jelszo;
+    }
+    public function get_felhasznalonev() {  
+        return $this->felhasznalonev;
+    }
+}
+//$tanulo = new ulesrend;
+
+//$tanulo->set_user(15,$conn);
+
+//echo $tanulo->get_nev();
+
+?>
