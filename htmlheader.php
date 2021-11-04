14601c46b334e2bedef2ae9c31e897522a7d3ba5
FazekasMiklos <miklos81006@gmail.com>
2021-11-04 09:00:24 +0100

    php

--- htmlheader.php
+++ htmlheader.php
@@ -1,16 +1,17 @@
 <?php
-if(!empty($_GET['logout'])){
-    session_unset();
+
+if(!empty($_GET['logout'])) {
+	session_unset();
 }
 
 ?>
 <!doctype html>
-<html lang="HU">
-
+<html lang="hu">
 <head>
-  <meta charset="utf-8">
-  <title><?php echo $title; ?></title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <link rel="stylesheet"  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<meta charset="utf-8">
+	<title><?php echo $title; ?></title>	
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 </head>
     
--- model/ulesrend.php
+++ model/ulesrend.php
@@ -56,7 +56,7 @@ class ulesrend {
   if($result = $conn->query($sql)){
 if($result->num_rows>0){
     while($row = $result->fetch_assoc()){
-        
+        $lista[] = $row['id'];
     }
 }
   }
