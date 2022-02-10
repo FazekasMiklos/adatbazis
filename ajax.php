87d51a863215836100224696ff70c13d7e6db606
FazekasMiklos <miklos81006@gmail.com>
2022-02-10 12:48:14 +0100

    ajax2

--- ajax.php
+++ ajax.php
@@ -4,7 +4,24 @@ require 'includes/db.inc.php';
 
 
 if(isset($_REQUEST['keres'])){
-    $sql = "SELECT `nev` FROM `ulesrend` WHERE `nev` LIKE '%' '".$_REQUEST['keres']."' '%'";
+
+    $sql = "SELECT `nev` FROM `ulesrend` WHERE `nev` LIKE ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $like);
+    $like = $_REQUEST['keres'].'%';
+    $stmt->execute();
+    if($result = $stmt->get_result()){
+    if ($result->num_rows > 0){
+        while($row=$result->fetch_assoc()){
+            echo $row['nev']."<br>";
+        }
+    }else
+echo "Nincs ilyen név";
+    }
+}
+/*
+if(isset($_REQUEST['keres'])){
+    $sql = "SELECT `nev` FROM `ulesrend` WHERE `nev` LIKE ' %' '".$_REQUEST['keres']."' '%'";
 
     if(!$result = $conn->query($sql))echo $conn->error;
     if ($result->num_rows > 0){
@@ -15,4 +32,5 @@ if(isset($_REQUEST['keres'])){
 echo "Nincs ilyen név";
 
 }
+*/
 ?>
