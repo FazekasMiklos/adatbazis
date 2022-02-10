fc6caf5005a63e047603d49cdb089d9f5b0c2f44
FazekasMiklos <miklos81006@gmail.com>
2022-02-10 10:55:16 +0100

    ajax

---
+++ ajax.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'includes/db.inc.php';
+
+
+if(isset($_REQUEST['keres'])){
+    $sql = "SELECT `nev` FROM `ulesrend` WHERE `nev` LIKE '%' '".$_REQUEST['keres']."' '%'";
+
+    if(!$result = $conn->query($sql))echo $conn->error;
+    if ($result->num_rows > 0){
+        while($row=$result->fetch_assoc()){
+            echo $row['nev']."<br>";
+        }
+    }else
+echo "Nincs ilyen név";
+
+}
+?>
